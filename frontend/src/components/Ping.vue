5839e9b53c8cc11815a1331f58be9d691c62b690
Antoine Scherrer <ascherrer@em-lyon.com>
2023-11-07 18:00:58 +0100

    update ping

--- frontend/src/components/Ping.vue
+++ frontend/src/components/Ping.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container">
-    <button
-      type="button"
-      class="btn btn-primary"
-    >
-      {{ msg }}
-    </button>
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
   </div>
 </template>
 
@@ -16,6 +11,22 @@ export default {
     return {
       msg: "Hello!",
     };
+  },
+  methods: {
+    getMessage() {
+      const path = "http://backend/ping";
+      this.$http
+        .get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  mounted() {
+    this.getMessage();
   },
 };
 </script>
