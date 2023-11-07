5f980aa7fa51e44dadffc9e348e4dae55c4f672f
Antoine Scherrer <ascherrer@em-lyon.com>
2023-11-07 17:46:10 +0100

    configure eslint

--- frontend/src/components/Ping.vue
+++ frontend/src/components/Ping.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <button type="button" class="btn btn-primary">{{ msg }}</button>
+    <button
+      type="button"
+      class="btn btn-primary"
+    >
+      {{ msg }}
+    </button>
   </div>
 </template>
 
