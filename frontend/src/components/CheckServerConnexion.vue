53a8db6d4f56ae3a637c8538349cc310bf8e7a73
Antoine Scherrer <ascherrer@em-lyon.com>
2023-11-08 09:32:31 +0100

    wip prediction form

---
+++ frontend/src/components/CheckServerConnexion.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="alert alert-primary" role="alert">{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckServerConnexion",
+  data() {
+    return {
+      msg: "Hello!",
+    };
+  },
+  mounted() {
+    this.getMessage();
+  },
+  methods: {
+    getMessage() {
+      const path = "/api/ping";
+      this.$http
+        .get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
